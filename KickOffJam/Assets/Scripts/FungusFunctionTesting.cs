601d184aae1feb40e224066341d4a244dc5af6b5
JustinCheung6 <justincheung6@gmail.com>
2020-09-25 14:17:49 -0700

    - Tested Fungus interacting with script (It worked)

---
+++ KickOffJam/Assets/Scripts/FungusFunctionTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusFunctionTesting : MonoBehaviour
+{
+
+    [SerializeField] private Flowchart FC = null;
+
+    [SerializeField] private int score = 0;
+
+    private void Update()
+    {
+        if(FC.GetBooleanVariable("Activate"))
+        {
+            score++;
+            FC.SetBooleanVariable("Activate", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("We in");
+
+        if (FC != null)
+            FC.ExecuteBlock("The Start of Stuff");
+    }
+}
