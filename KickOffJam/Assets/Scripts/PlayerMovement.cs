8ff41342e75c71de453ace809c97dddf1bedf1fe
Britney Khuu <britneykhuu@gmail.com>
2020-09-25 13:00:18 -0700

    made simple transform movement code; removed camera folder and things related to it

--- KickOffJam/Assets/Scripts/PlayerMovement.cs
+++ KickOffJam/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //public GameObject player;
+    public int moveSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //Moves Forward and back along y axis                           //Up/Down
+        transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical")* moveSpeed);
+        //Moves Left and right along x Axis                               //Left/Right
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal")* moveSpeed); 
     }
+    
     
 }
